64d442b2b503359a6a48faf2ef4901cafd51d4a7
Adrian Raflyandri <adrianrafly.ar@gmail.com>
2025-06-02 14:31:10 +0700

    Add config.properties

--- src/test/java/com/test/OrangeHRMTestSaucelab.java
+++ src/test/java/com/test/OrangeHRMTestSaucelab.java
@@ -1,11 +1,17 @@
 package com.test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -13,14 +19,34 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class OrangeHRMTest {
+public class OrangeHRMTestSaucelab {
 //	public static ChromeDriver;
 	public static RemoteWebDriver driver;
 	public static WebDriverWait wait;
 
 	@BeforeClass
 	public void setUp() throws MalformedURLException {
-		driver = new ChromeDriver();
+//		driver = new ChromeDriver();
+		ChromeOptions browserOptions = new ChromeOptions();
+		browserOptions.setPlatformName("Windows 11");
+		browserOptions.setBrowserVersion("latest");
+
+		Properties props = new Properties();
+		try (FileInputStream fis = new FileInputStream("path/to/config.properties")) {
+			props.load(fis);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Map<String, Object> sauceOptions = new HashMap<>();
+		sauceOptions.put("username", props.getProperty("usernameSaucelab"));
+		sauceOptions.put("accessKey", props.getProperty("accessKey"));
+		sauceOptions.put("build", props.getProperty("build"));
+		sauceOptions.put("name", "Sanity Test");
+		browserOptions.setCapability("sauce:options", sauceOptions);
+
+		// start the session
+		URL url = new URL("https://ondemand.us-west-1.saucelabs.com:443/wd/hub");
+		driver = new RemoteWebDriver(url, browserOptions);
 
 		driver.manage().window().maximize();
 		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
